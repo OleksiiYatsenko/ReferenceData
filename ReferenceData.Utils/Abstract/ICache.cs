79c4e7707a2da602fe077d1922608310eaf3f189
Vitalii Morenko <Vitalii_Morenko@epam.com>
2015-07-13 17:08:15 +0300

    Added lost file during last commit

---
+++ ReferenceData.Utils/Abstract/ICache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceData.Utils.Abstract
+{
+    public interface ICache<TKey, TValue>
+    {
+        void Put(TKey key, TValue value);
+        void PutAll(IEnumerable<KeyValuePair<TKey, TValue>> values);
+        TValue Get(TKey key);
+        IEnumerable<TValue> GetAll();
+        bool Contains(TKey key);
+    }
+}
---
+++ ReferenceData.Utils/Concrete/MemoryCache.cs
@@ -0,0 +1,40 @@
+﻿using ReferenceData.Utils.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceData.Utils.Concrete
+{
+    public class MemoryCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> storageDictionary = new Dictionary<TKey, TValue>();
+
+        public void Put(TKey key, TValue value)
+        {
+            storageDictionary[key] = value;
+        }
+
+        public void PutAll(IEnumerable<KeyValuePair<TKey, TValue>> values)
+        {
+            foreach (var value in values)
+                storageDictionary[value.Key] = value.Value;
+        }
+
+        public TValue Get(TKey key)
+        {
+            return storageDictionary[key];
+        }
+
+        public IEnumerable<TValue> GetAll()
+        {
+            return storageDictionary.Values;
+        }
+
+        public bool Contains(TKey key)
+        {
+            return storageDictionary.ContainsKey(key);
+        }
+    }
+}
