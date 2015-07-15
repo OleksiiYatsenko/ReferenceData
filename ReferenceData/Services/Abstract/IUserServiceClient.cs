dda620005e3ad87e5a144afa4c1c1a6006dc742c
Vitalii Morenko <Vitalii_Morenko@epam.com>
2015-07-15 10:21:46 +0300

    Added client-side user service

---
+++ ReferenceData/Services/Abstract/IUserServiceClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceData.Services.Abstract
+{
+    public interface IUserServiceClient
+    {
+        IEnumerable<UserFullInfo> GetUsers();
+        UserFullInfo AddOrUpdate(UserFullInfo user);
+        UserFullInfo GetUser(int id);
+    }
+}
---
+++ ReferenceData/Services/UserServiceClient.cs
@@ -0,0 +1,40 @@
+﻿using EmitMapper;
+using ReferenceData.Model;
+using ReferenceData.Services.Abstract;
+using ReferenceData.UserServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceData.Services
+{
+    class UserServiceClient : IUserServiceClient
+    {
+        private IUsersService userService;
+
+        public UserServiceClient(IUsersService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IEnumerable<UserFullInfo> GetUsers()
+        {
+            foreach (var user in userService.GetUsers())
+                yield return ObjectMapperManager.DefaultInstance.GetMapper<User, UserFullInfo>(App.ConfigUserFullInfo).Map(user);
+        }
+
+        public UserFullInfo AddOrUpdate(UserFullInfo user)
+        {
+            User usr = ObjectMapperManager.DefaultInstance.GetMapper<UserFullInfo, User>(App.ConfigUserFullInfo).Map(user);
+            usr = userService.AddOrUpdate(usr);
+            return ObjectMapperManager.DefaultInstance.GetMapper<User, UserFullInfo>(App.ConfigUserFullInfo).Map(usr);
+        }
+
+        public UserFullInfo GetUser(int id)
+        {
+            return ObjectMapperManager.DefaultInstance.GetMapper<User, UserFullInfo>(App.ConfigUserFullInfo).Map(userService.GetItem(id));
+        }
+    }
+}
