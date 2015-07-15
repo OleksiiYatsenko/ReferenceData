aeabbd273fabe76bf8e289d86a3a31b8a6f6c67a
OleksiiYatsenko <forsir2000@gmail.com>
2015-07-15 13:23:17 +0300

    Add max count of records in combobox

---
+++ ReferenceData/Controls/ComboBoxMaxCount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ReferenceData
+{
+    class ComboBoxMaxCount : ComboBox
+    {
+        public static int GetMaxDropDownItems(DependencyObject obj)
+        {
+            return (int)obj.GetValue(MaxDropDownItemsProperty);
+        }
+        public static void SetMaxDropDownItems(DependencyObject obj, int value)
+        {
+            obj.SetValue(MaxDropDownItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty MaxDropDownItemsProperty =
+            DependencyProperty.RegisterAttached("MaxDropDownItems", typeof(int), typeof(ComboBoxMaxCount), new PropertyMetadata
+            {
+                //обратный вызов, производимый в случае изменения свойства
+                PropertyChangedCallback = (obj, e) =>
+                {
+                    var box = (ComboBox)obj;
+                    box.DropDownOpened += UpdateHeight;
+                    if (box.IsDropDownOpen)
+                        UpdateHeight(box, null);
+                }
+            });
+
+        static void UpdateHeight(object sender, EventArgs e)
+        {
+            var box = (ComboBox)sender;
+            box.Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() =>
+            //здесь мы описываем, что будет происходить
+            {
+                var container = box.ItemContainerGenerator.ContainerFromIndex(0) as UIElement;
+                if (container != null && container.RenderSize.Height > 0)
+                    box.MaxDropDownHeight = container.RenderSize.Height * GetMaxDropDownItems(box);
+            }));
+        }
+    }
+}
